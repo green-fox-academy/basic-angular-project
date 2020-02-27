5544cb9474d87526a774acce010c3e78774b95e9
István Kárász <ikarasz@outlook.com>
2020-02-27 17:42:21 +0100

    build(linter): fix errors

--- src/app/services/api/api.service.spec.ts
+++ src/app/services/api/api.service.spec.ts
@@ -7,15 +7,15 @@ describe('ApiService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: ApiService = TestBed.get(ApiService);
+    const service: ApiService = TestBed.inject(ApiService);
     expect(service).toBeTruthy();
   });
 
   it('should return dummy content', (done: DoneFn) => {
-    const service: ApiService = TestBed.get(ApiService);
+    const service: ApiService = TestBed.inject(ApiService);
     service.getTitle().subscribe(value => {
       expect(value).toEqual({ title: '¡Hola Jorge!' });
       done();
-    })
+    });
   });
 });
